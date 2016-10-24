487fcfd2a8cc6b4d61b7ff8761510462339d61e9
MaxProvin <maxprovin01@gmail.com>
2016-10-24 16:46:14 +0100

    Hello World!

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
 
 router.get('/', function(req, res) {
+	res.render('landingPage/index')
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
-var routes = require('routes');
+var routes = require('./app/routes');
 
 // Morgan is a module that outputs request response information to the console while the server is running
 var logger = require('morgan');
 
+app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.use(express.static('app/public'));
 app.use(logger('dev'));
-
 app.use('/', routes);
 
 
@@ -19,5 +19,5 @@ var server = app.listen(4000, function () {
    var host = server.address().address
    var port = server.address().port
 
-   console.log("database_server listening at localhost:", host, port)
+   console.log("database_server listening at localhost:", port)
 })
