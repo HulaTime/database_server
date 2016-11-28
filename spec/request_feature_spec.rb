4976d87a7400338e8853d6b40e922b026486b609
Max Provin <maxprovin@outlook.com>
2016-11-28 15:19:11 +0000

    fixed rspec core conflict

--- spec/request_feature_spec.rb
+++ spec/request_feature_spec.rb
@@ -1,5 +1,5 @@
-describe 'Get request', :type => :feature do
-  it 'set a key, value pair and access it' do
+feature 'Get request' do
+  scenario 'set a key, value pair and access it' do
     visit('/set?name=max')
     visit('/get?key=name')
     expect(page).to have_content 'max'
